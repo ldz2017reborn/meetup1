f6ed9f842ec435649ff3ed6951f1f10ccdd53851
Your Name <you@example.com>
2017-07-29 15:24:02 +0800

    change wrong controller name into right

--- app/controllers/api/v1/users_controller.rb_controller.rb
+++
@@ -1,5 +0,0 @@
-class Api::V1::UsersController.rbController < ApplicationController
-  def show
-    @user = User.find(params[:id])
-  end
-end
--- app/views/api/v1/users_controller.rb/show.json.jbuilder
+++
@@ -1,3 +0,0 @@
-json.user do
-  json.(@user, :id, :email, :name, :activated, :admin, :created_at, :updated_at)
-end
